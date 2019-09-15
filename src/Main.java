b4ecfa1a84cde1368979af4c5083660efd7729c2
MaratArkhipov <maratkzn2010@yandex.ru>
2019-09-16 00:30:04 +0400

    расставила пробелы

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,10 @@
 
 public class Main {
     public static void main(String[] args) {
-        int distanceKm=100; // расстояние, на которое будет задан расход топлива
-        float fuelConsumptionAtADist=145.8f; // ввести расход топлива
-        float fuelAvailable=125.9f; //ввести имеющийся объем топлива
-        float allowableDistance= distanceKm*fuelConsumptionAtADist/fuelAvailable;
+        int distanceKm = 100; // расстояние, на которое будет задан расход топлива
+        float fuelConsumptionAtADist = 145.8f; // ввести расход топлива
+        float fuelAvailable = 125.9f; //ввести имеющийся объем топлива
+        float allowableDistance = distanceKm * fuelConsumptionAtADist / fuelAvailable;
         System.out.println(allowableDistance);
     }
 }
