977a4d7d796c4ac08aa55250a0bfdc744238e334
MaratArkhipov <maratkzn2010@yandex.ru>
2019-09-15 23:09:47 +0400

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+// Юлия Артес д/з 1_1, расчет расхода топлива
+
+public class Main {
+    public static void main(String[] args) {
+        int distanceKm=100; // расстояние, на которое будет задан расход топлива
+        float fuelConsumptionAtADist=145.8f; // ввести расход топлива
+        float fuelAvailable=125.9f; //ввести имеющийся объем топлива
+        float allowableDistance= distanceKm*fuelConsumptionAtADist/fuelAvailable;
+        System.out.println(allowableDistance);
+    }
+}
