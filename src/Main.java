3a2fb2c6f9100b96ac71c133b3c0ea25ede42f66
MaratArkhipov <maratkzn2010@yandex.ru>
2019-09-24 23:46:42 +0400

    fixes#4

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,9 @@
 
 public class Main {
     public static void main(String[] args) {
-        int distanceKm = 100; //distance over which fuel consumption will be set
-        float fuelConsumptionAtADist = 145.8f; // enter fuel consumption
-        float fuelAvailable = 125.9f; //enter the available fuel volume
+        int distanceKm = 100;
+        float fuelConsumptionAtADist = 145.8f;
+        float fuelAvailable = 125.9f;
         float allowableDistance = distanceKm * fuelConsumptionAtADist / fuelAvailable;
         System.out.println(allowableDistance);
     }
